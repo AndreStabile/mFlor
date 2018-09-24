bad7b150c1fa22a132a7493f1840a7f62627adec
André Stábile <arquiduqueandre@gmail.com>
2018-09-23 23:58:42 -0300

    WIP: Ruby tutorial

--- app/controllers/pecas_controller.rb
+++ app/controllers/pecas_controller.rb
@@ -1,8 +1,49 @@
 class PecasController < ApplicationController
+  def index
+    @peca = Peca.all
+  end
+  
   def new
+    @peca = Peca.new
+  end
+
+  def edit
+    @peca = Peca.find(params[:id])
   end
 
   def create
-    render plain: params[:article].inspect
+    @peca = Peca.new(peca_params)
+ 
+    if @peca.save
+      redirect_to @peca
+    else
+      render 'new'
+    end
   end
+
+  def update
+    @peca = Peca.find(params[:id])
+   
+    if @peca.update(peca_params)
+      redirect_to @peca
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @peca = Peca.find(params[:id])
+  end
+
+  def destroy
+    @peca = Peca.find(params[:id])
+    @peca.destroy
+   
+    redirect_to pecas_path
+  end
+
+  private
+    def peca_params
+      params.require(:peca).permit(:title, :text)
+    end
 end
---
+++ app/models/peca.rb
@@ -0,0 +1,4 @@
+class Peca < ApplicationRecord
+  validates :title, presence: true,
+                    length: { minimum: 5 }
+end
